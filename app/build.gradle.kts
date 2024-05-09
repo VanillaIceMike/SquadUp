4d16117302b3d3091eeaeefff731f17f1fc08363
Callum Sharp <callum@the-sharps.me.uk>
2024-05-09 01:55:12 -0700

    game posts displayed on homescreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,5 @@ dependencies {
     implementation ("com.github.dhaval2404:imagepicker:2.1")
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
-    implementation ("com.github.bumptech.glide:compiler:4.15.1'")
 }
 
