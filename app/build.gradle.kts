c563b1dcd8cc7c9a3a386acc193a3e3e3dda7a3e
Michael Zabaneh <michael.zabaneh@sjsu.edu>
2024-05-10 01:16:41 -0700

    changed colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,5 +89,7 @@ dependencies {
     implementation ("com.github.dhaval2404:imagepicker:2.1")
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
+    implementation ("com.google.android.material:material:1.3.0")
+
 }
 
